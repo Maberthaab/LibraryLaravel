579c5a84e0a642dcf5d9aefe9333eb50bbbc45fc
Maberthaab <thataaberlian@gmail.com>
2023-07-23 19:28:20 +0700

    first commit

---
+++ resources/views/banner.blade.php
@@ -0,0 +1,3 @@
+<div class=" col-md-12">
+    <img src="{{ asset('img') }}/banner.jpeg" width="100%" height="280px">
+</div>
---
+++ resources/views/view_home.blade.php
@@ -0,0 +1,10 @@
+@extends('index')
+@section('title', 'Home')
+
+@section('isihalaman')
+        <h3> About Perpustakaan </h3>
+        <p>
+            <h4> Perpustakaan </h4>
+            Perpustakaan 
+
+@endsection
